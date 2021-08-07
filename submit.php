773a82b1b6da90e14f0b6896b22055b1bb8cb8d8
Luke <uklukehemmings@gmail.com>
2021-08-07 12:23:37 +0100

    initial commit of form processing

---
+++ submit.php
@@ -0,0 +1,48 @@
+<?php
+
+$first_name = htmlspecialchars(trim($_POST['first_name']));
+$last_name 	= htmlspecialchars(trim($_POST['last_name']));
+$email 		= htmlspecialchars(trim($_POST['email']))
+$tel 		= htmlspecialchars(trim($_POST['tel']))
+$address1	= htmlspecialchars(trim($_POST['address1']));
+$address2	= htmlspecialchars(trim($_POST['address2']));
+$town 		= htmlspecialchars(trim($_POST['town']))
+$county 	= htmlspecialchars(trim($_POST['county']))
+$postcode	= htmlspecialchars(trim($_POST['postcode']));
+$country	= htmlspecialchars(trim($_POST['country']));
+$description= htmlspecialchars(trim($_POST['description']));
+
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file_to_upload"]["name"]);
+$upload_check = 1;
+$file_extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $upload_check = 0;
+}
+
+if ($_FILES["file_to_upload"]["size"] > 1000000) {
+  echo "Sorry, your file is too large.";
+  $upload_check = 0;
+}
+
+if($file_extension != "doc" && $file_extension != "docx" && $file_extension != "txt"
+&& $file_extension != "pdf" ) {
+  echo "Please select a file with the extension DOC, DOCX, TXT or PDF";
+  $upload_check = 0;
+}
+
+if ($upload_check == 0) {
+	echo "Sorry, your file could not be uploaded.";
+} else {
+	if (move_uploaded_file($_FILES["file_to_upload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["file_to_upload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+
+}
+
+?>
