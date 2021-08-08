e7da17d0f5a2ea20840866c22d07a6b0039d5a55
Luke <uklukehemmings@gmail.com>
2021-08-08 21:06:35 +0100

    improved response formatting

--- submit.php
+++ submit.php
@@ -15,6 +15,7 @@ $description= htmlspecialchars(trim($_POST['description']));
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["file_to_upload"]["name"]);
+$public_target_file = substr($target_file, strlen($target_dir));  
 $upload_check = 1;
 $file_extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -28,7 +29,7 @@ if ($_FILES["file_to_upload"]["size"] > 1000000) {
   $upload_check = 0;
 }
 
-if($file_extension != "doc" && $file_extension != "docx" && $file_extension != "txt" && $file_extension != "pdf" ) {
+if ($file_extension != "doc" && $file_extension != "docx" && $file_extension != "txt" && $file_extension != "pdf" ) {
   echo "Please select a file with the extension DOC, DOCX, TXT or PDF. ";
   $upload_check = 0;
 }
@@ -37,7 +38,7 @@ if ($upload_check == 0) {
 	echo "Sorry, your file could not be uploaded. ";
 } else {
 	if (move_uploaded_file($_FILES["file_to_upload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["file_to_upload"]["name"])). " has been uploaded. ";
+    echo " Success! The file ". htmlspecialchars( basename( $_FILES["file_to_upload"]["name"])). " has been uploaded. ";
   } else {
     echo "Sorry, there was an error uploading your file. ";
   }
@@ -46,25 +47,32 @@ if ($upload_check == 0) {
 
 $to = $email;
 $subject = "Confirmation of submission";
-$headers = "From: hr@corporate.com" . "\r\n" .
+$headers = "From: hr@corporate.com" . "\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
 
 $msg = <<<MSG
-Hello!
 You submitted the following information in to our web form: <br>
-First name: $first_name <br>
-Last name: $last_name <br>
-Email: $email    <br>
-Telephone: $tel      <br>
-Address 1: $address1 <br>
-Address 2: $address2 <br>
-Town: $town     <br>
-County: $county   <br>
-Postcode: $postcode <br>
-Country: $country  <br>
+<table>
+<tr><td>First name: </td><td>$first_name</td></tr>
+<tr><td>Last name: </td><td>$last_name </td></tr>
+<tr><td>Email: </td><td>$email    </td></tr>
+<tr><td>Telephone: </td><td>$tel      </td></tr>
+<tr><td>Address 1: </td><td>$address1 </td></tr>
+<tr><td>Address 2: </td><td>$address2 </td></tr>
+<tr><td>Town: </td><td>$town     </td></tr>
+<tr><td>County: </td><td>$county   </td></tr>
+<tr><td>Postcode: </td><td>$postcode </td></tr>
+<tr><td>Country: </td><td>$country  </td></tr>
+<tr><td>CV file uploaded: </td><td>$public_target_file </td></tr> 
+</table>
 MSG;
 
-if(mail($to, $subject, $msg, $headers)) {
-    echo "Email sent.";
+echo $msg;
+
+if(mail($to, $subject, "<html><body>" .$msg. "</html></body>", $headers)) {
+    echo "A confirmation email containing this information has been sent to the email address $email. ";
 }
+
 
 ?>
