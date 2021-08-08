013ff844ed12200131126fbfcd69096079f34b82
Luke <uklukehemmings@gmail.com>
2021-08-08 21:18:00 +0100

    added a back button when the file coul not be uploaded

--- submit.php
+++ submit.php
@@ -20,12 +20,12 @@ $upload_check = 1;
 $file_extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "Sorry, file already exists. ";
   $upload_check = 0;
 }
 
 if ($_FILES["file_to_upload"]["size"] > 1000000) {
-  echo "Sorry, your file is too large.";
+  echo "Sorry, your file is too large. ";
   $upload_check = 0;
 }
 
@@ -35,43 +35,47 @@ if ($file_extension != "doc" && $file_extension != "docx" && $file_extension != 
 }
 
 if ($upload_check == 0) {
-	echo "Sorry, your file could not be uploaded. ";
+	echo 'Sorry, your file could not be uploaded. <form><input type="button" value="Go back" onclick="history.back()"></form>';
 } else {
 	if (move_uploaded_file($_FILES["file_to_upload"]["tmp_name"], $target_file)) {
     echo " Success! The file ". htmlspecialchars( basename( $_FILES["file_to_upload"]["name"])). " has been uploaded. ";
   } else {
     echo "Sorry, there was an error uploading your file. ";
+
   }
 
 }
 
-$to = $email;
-$subject = "Confirmation of submission";
-$headers = "From: hr@corporate.com" . "\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+if ($upload_check == 1) {
+  $to = $email;
+  $subject = "Confirmation of submission";
+  $headers = "From: hr@corporate.com" . "\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
-$msg = <<<MSG
-You submitted the following information in to our web form: <br>
-<table>
-<tr><td>First name: </td><td>$first_name</td></tr>
-<tr><td>Last name: </td><td>$last_name </td></tr>
-<tr><td>Email: </td><td>$email    </td></tr>
-<tr><td>Telephone: </td><td>$tel      </td></tr>
-<tr><td>Address 1: </td><td>$address1 </td></tr>
-<tr><td>Address 2: </td><td>$address2 </td></tr>
-<tr><td>Town: </td><td>$town     </td></tr>
-<tr><td>County: </td><td>$county   </td></tr>
-<tr><td>Postcode: </td><td>$postcode </td></tr>
-<tr><td>Country: </td><td>$country  </td></tr>
-<tr><td>CV file uploaded: </td><td>$public_target_file </td></tr> 
-</table>
-MSG;
+  $msg = <<<MSG
+  You submitted the following information in to our web form: <br>
+  <table>
+  <tr><td>First name: </td><td>$first_name</td></tr>
+  <tr><td>Last name: </td><td>$last_name </td></tr>
+  <tr><td>Email: </td><td>$email    </td></tr>
+  <tr><td>Telephone: </td><td>$tel      </td></tr>
+  <tr><td>Address 1: </td><td>$address1 </td></tr>
+  <tr><td>Address 2: </td><td>$address2 </td></tr>
+  <tr><td>Town: </td><td>$town     </td></tr>
+  <tr><td>County: </td><td>$county   </td></tr>
+  <tr><td>Postcode: </td><td>$postcode </td></tr>
+  <tr><td>Country: </td><td>$country  </td></tr>
+  <tr><td>Description: </td><td>$description  </td></tr>
+  <tr><td>CV file uploaded: </td><td>$public_target_file </td></tr> 
+  </table>
+  MSG;
 
-echo $msg;
+  echo wordwrap($msg,70);
 
-if(mail($to, $subject, "<html><body>" .$msg. "</html></body>", $headers)) {
-    echo "A confirmation email containing this information has been sent to the email address $email. ";
+  if(mail($to, $subject, "<html><body>" .$msg. "</html></body>", $headers)) {
+      echo "A confirmation email containing this information has been sent to the email address $email. ";
+  }
 }
 
 
